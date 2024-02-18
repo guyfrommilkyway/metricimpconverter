4901f89e601a74e3e92e491e1e0039892cfe5238
Almer Tampus <almerflorestampus@gmail.com>
2024-02-19 05:11:35 +0800

    feat: setup /api/convert with dummy data

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,25 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+	let convertHandler = new ConvertHandler();
 
+	app.get('/api/convert', (req, res) => {
+		// const result = convertHandler();
+		const { input } = req?.query;
+
+		console.log(input.match(/\d+(\.\d+)?\/\d+(\.\d+)?|\d+(\.\d+)?/g));
+
+		const initNum = 12;
+		const initUnit = 'kg';
+		const returnNum = 12;
+		const returnUnit = 'lbs';
+
+		res.status(200).json({
+			initNum,
+			initUnit,
+			returnNum,
+			returnUnit,
+			string: `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`,
+		});
+	});
 };
