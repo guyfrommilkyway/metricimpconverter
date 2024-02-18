90e7c6f248f681547795d544cf5f4a62601b712a
Almer Tampus <almerflorestampus@gmail.com>
2024-02-19 05:23:40 +0800

    test: add reading decimal number input

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,7 +5,7 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function () {
-	suite('Read input', function () {
+	suite('Reading input', function () {
 		test('Should correctly read a whole number input', function () {
 			assert.equal(convertHandler.getNum('12kg'), 12);
 			assert.equal(convertHandler.getNum('100lbs'), 100);
@@ -13,6 +13,15 @@ suite('Unit Tests', function () {
 			assert.equal(convertHandler.getNum('1L'), 1);
 			assert.equal(convertHandler.getNum('40km'), 40);
 			assert.equal(convertHandler.getNum('1mi'), 1);
+		});
+
+		test('Should correctly read a decimal number input', function () {
+			assert.equal(convertHandler.getNum('12.1kg'), 12.1);
+			assert.equal(convertHandler.getNum('100.50lbs'), 100.5);
+			assert.equal(convertHandler.getNum('4.4gal'), 4.4);
+			assert.equal(convertHandler.getNum('1.12L'), 1.12);
+			assert.equal(convertHandler.getNum('40.5km'), 40.5);
+			assert.equal(convertHandler.getNum('1.8mi'), 1.8);
 		});
 	});
 });
