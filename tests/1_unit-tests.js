c542d560cf18976cc006cc6a4056e2432ebe977f
Almer Tampus <almerflorestampus@gmail.com>
2024-02-19 05:10:57 +0800

    test: add reading a whole number input

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,15 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite('Unit Tests', function () {
+	suite('Read input', function () {
+		test('Should correctly read a whole number input', function () {
+			assert.equal(convertHandler.getNum('12kg'), 12);
+			assert.equal(convertHandler.getNum('100lbs'), 100);
+			assert.equal(convertHandler.getNum('4gal'), 4);
+			assert.equal(convertHandler.getNum('1L'), 1);
+			assert.equal(convertHandler.getNum('40km'), 40);
+			assert.equal(convertHandler.getNum('1mi'), 1);
+		});
+	});
 });
